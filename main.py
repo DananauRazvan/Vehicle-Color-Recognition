83a5ec0c6c4ba5c48b7098035685a9c86a77cc0a
DananauRazvan <73775639+DananauRazvan@users.noreply.github.com>
2021-07-21 10:48:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,30 +4,29 @@ import keras
 import tensorflow
 from imageai.Detection import ObjectDetection
 from PIL import Image
-from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-import numpy
+import cv2
 
-def vehicleDetector(input_path, output_path):
+def vehicleDetector(inputPath, outputPath):
     detector = ObjectDetection()
 
-    model_path = 'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/yolo.h5'
+    modelPath = 'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/yolo.h5'
 
     detector.setModelTypeAsYOLOv3()
-    detector.setModelPath(model_path, )
+    detector.setModelPath(modelPath, )
     detector.loadModel(detection_speed = 'flash')
 
-    for imagePath in glob.glob(input_path):
-        returned_image, detection = detector.detectObjectsFromImage(input_image = imagePath, output_type = 'array')
-        max_percentage_probability = 0
+    for imagePath in glob.glob(inputPath):
+        returnedImage, detection = detector.detectObjectsFromImage(input_image = imagePath, output_type = 'array')
+        maxPercentageProbability = 0
         index = -1
         i = -1
         ok = False
         for eachItem in detection:
             i += 1
-            if (eachItem['name'] == 'car' or eachItem['name'] == 'truck' or eachItem['name'] == 'motorcycle') and eachItem['percentage_probability'] > max_percentage_probability:
+            if (eachItem['name'] == 'car' or eachItem['name'] == 'truck' or eachItem['name'] == 'motorcycle') and eachItem['percentage_probability'] > maxPercentageProbability:
                 index = i
-                max_percentage_probability = eachItem['percentage_probability']
+                maxPercentageProbability = eachItem['percentage_probability']
                 ok = True
 
         if ok:
@@ -35,92 +34,77 @@ def vehicleDetector(input_path, output_path):
             image = Image.open(imagePath)
             box = detection[index]['box_points']
             image = image.crop(box)
-            image.save(output_path + imagePath[71:])
+            image.save(outputPath + imagePath[71:])
         else:
             print('undetectable')
             image = Image.open(imagePath)
-            image.save(output_path + imagePath[71:])
+            image.save(outputPath + imagePath[71:])
             continue
 
-def input(input_path_train_images, input_path_train_labels, input_path_test_images, input_path_test_labels):
-    vehicleDetector('C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/train/*.jpg',
-                    'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTrain/')
-    vehicleDetector('C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/test/*.jpg',
-                    'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTest/')
+def principalComponentAnalysis(inputPath, outputPath):
+    for imagePath in glob.glob(inputPath):
+        image = cv2.imread(imagePath)
 
+        red, green, blue = cv2.split(image)
+
+        dfRed = red / 255
+        dfBlue = blue / 255
+        dfGreen = green / 255
+
+        pcaRed = PCA(n_components = 10)
+        pcaRed.fit(dfRed)
+        transPcaRed = pcaRed.transform(dfRed)
+        pcaBlue = PCA(n_components = 10)
+        pcaBlue.fit(dfBlue)
+        transPcaBlue = pcaBlue.transform(dfBlue)
+        pcaGreen = PCA(n_components = 10)
+        pcaGreen.fit(dfGreen)
+        transPcaGreen = pcaGreen.transform(dfGreen)
+
+        redArr = pcaRed.inverse_transform(transPcaRed)
+        blueArr = pcaBlue.inverse_transform(transPcaBlue)
+        greenArr = pcaGreen.inverse_transform(transPcaGreen)
+
+        imageReduced = (cv2.merge((redArr, greenArr, blueArr)))
+
+        if inputPath == 'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTrain/*.jpg':
+            name = imagePath[75:]
+        else:
+            name = imagePath[74:]
+
+        cv2.imwrite(outputPath + name, imageReduced * 255)
+
+def input(inputPathTrainImages, inputPathTrainLabels, inputPathTestImages, inputPathTestLabels):
     trainImages = []
-    for imagePath in glob.glob(input_path_train_images):
+    for imagePath in glob.glob(inputPathTrainImages):
         image = imageio.imread(imagePath)
         trainImages.append(image)
 
     trainLabels = []
-    f = open(input_path_train_labels, 'r')
+    f = open(inputPathTrainLabels, 'r')
     lines = f.readlines()
     for line in lines:
         trainLabels.append(int(line))
 
     testImages = []
-    for imagePath in glob.glob(input_path_test_images):
+    for imagePath in glob.glob(inputPathTestImages):
         image = imageio.imread(imagePath)
         testImages.append(image)
 
     testLabels = []
-    f = open(input_path_test_labels, 'r')
+    f = open(inputPathTestLabels, 'r')
     lines = f.readlines()
     for line in lines:
         testLabels.append(int(line))
 
     return trainImages, trainLabels, testImages, testLabels
 
+vehicleDetector('C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/train/*.jpg',
+                'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTrain/')
+vehicleDetector('C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/test/*.jpg',
+                'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTest/')
 
-def principalComponentAnalysis():
-    trainImages, trainLabels, testImages, testLabels = input('C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTrain/*jpg',
-                                                             'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/trainLabel.txt',
-                                                             'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTest/*.jpg',
-                                                             'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/testLabel.txt')
-
-    size = 0
-    for i in range(len(trainImages)):
-        if len(trainImages[i]) > size:
-            size = len(trainImages[i])
-
-    for i in range(len(trainImages)):
-        aux = numpy.array(trainImages[i])
-        aux.resize(size)
-        trainImages[i] = aux
-
-    stdTrainImages = numpy.array(trainImages)
-    stdTrainImages = StandardScaler().fit_transform(stdTrainImages)
-
-    pcaTrainImages = PCA(n_components=10)
-    pcaTrainImages = pcaTrainImages.fit_transform(stdTrainImages)
-
-    """
-    covTrainImages = numpy.cov(stdTrainImages.T)
-
-    eigValsTrainImages, eigVecsTrainImages = numpy.linalg.eig(covTrainImages)
-
-    total = sum(eigValsTrainImages)
-    varExp = [(i / total) * 100 for i in sorted(eigValsTrainImages, reverse = True)]
-    cumVarExp = numpy.cumsum(varExp)
-    """
-
-    size = 0
-    for i in range(len(testImages)):
-        if len(testImages[i]) > size:
-            size = len(testImages[i])
-
-    for i in range(len(testImages)):
-        aux = numpy.array(testImages[i])
-        aux.resize(size)
-        testImages[i] = aux
-
-    stdTestImages = numpy.array(testImages)
-    stdTestImages = StandardScaler().fit_transform(stdTestImages)
-
-    pcaTestImages = PCA(n_components = 10)
-    pcaTestImages = pcaTestImages.fit_transform(stdTestImages)
-
-    return pcaTrainImages, pcaTestImages
-
-pcaTrainImages, pcaTestImages = principalComponentAnalysis()
+principalComponentAnalysis('C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTrain/*.jpg',
+                           'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTrainPCA/')
+principalComponentAnalysis('C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTest/*.jpg',
+                           'C:/Users/razva/OneDrive/Desktop/Vehicle Color Recognition/dataset/newTestPCA/')
